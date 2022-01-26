09d3cfcd8a1f44eccb9a1383d214081b677a3ac7
anuchax2root <98440044+anuchax2root@users.noreply.github.com>
2022-01-26 11:08:23 +0700

    Update menu.py
    update hello1-4

--- menu.py
+++ menu.py
@@ -1,16 +1,8 @@
 print('manin menu')
 print('hello')
-
-git config --global user.email "ratthaslip.booster@gmail.com"
-git config --global user.email "iotinvationlab"
-
-echo "# test" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/iotinvationlab/test.git
-git push -u origin main
-
+print('hello1')
+print('hello2')
+print('hello3')
+print('hello4')
 
 
